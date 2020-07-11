f2b54c5dacd009253d647ca0f84da9a29a2b142e
Johandre Delgado <johanesweb@gmail.com>
2020-07-11 10:00:05 +0300

    Add initial screen with title and list

--- BirdUI/BirdUI/PostListView.swift
+++ BirdUI/BirdUI/PostListView.swift
@@ -9,15 +9,43 @@
 import SwiftUI
 
 struct PostListView: View {
+  @ObservedObject var posts: PostViewModel = PostViewModel()
+
   var body: some View {
     // TODO: This should look exactly like the Birdie table view,
     // but with only one button.
-    Text("Layout header, new-post button, List of posts")
+    VStack(alignment:.leading, spacing: 10) {
+      HStack {
+        Image("mascot_swift-badge")
+          .resizable()
+          .scaledToFit()
+          .frame(height: 50)
+        Spacer()
+        Text("Home")
+          .font(.system(size: 44))
+          .offset(x: -25)
+        Spacer()
+
+      }
+      HStack {
+        Button("Create New Post") {
+          print("btn pressed.")
+        }
+        Spacer()
+      }
+      List(posts.posts) { post in
+        Text(post.textBody!)
+      }
+      Spacer()
+    }
+    .padding()
   }
 }
 
 struct PostListView_Previews: PreviewProvider {
   static var previews: some View {
-    PostListView()
+    NavigationView {
+      PostListView(posts: PostViewModel())
+    }
   }
 }
